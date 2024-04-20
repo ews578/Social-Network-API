3df280f2c5e32c77602b16e500d3eded5cd39689
ews578 <ewsnider28@yahoo.com>
2024-04-20 11:15:54 -0500

    set up folder finished user.js

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+      username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true
+    },
+    email: {
+      ttype: String,
+      unique: true,
+      required: true,
+      trimmed: true
+    },
+    
+  
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought"
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+userSchema.virtual("friendCount").get(function(){
+  return this.friends.length
+})
+const User = model('user', userSchema);
+
+module.exports = User;
